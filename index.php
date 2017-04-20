c1cbaff317c4b51a7d0879261a79f972b5a56cf8
Gareth Hughes <g_github@flamescape.com>
2017-04-20 18:17:52 +0100

    work stuff

--- index.php
+++ index.php
@@ -1,104 +1,103 @@
 <!DOCTYPE html>
-<html ng-app="quoteApp" >
+<html ng-app="quoteApp">
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
-<link rel="stylesheet" href="css/damasco-style.css"  type="text/css"/>
+<link rel="stylesheet" href="css/damasco-style.css" type="text/css" />
 <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css" />
 <link rel="stylesheet" type="text/css" href="css/dateInput.css" />
+
 <body ng-controller="styleController as style">
 
-<style>
-	.container{
+    <style>
+        .container {
+            padding-top: 50px
+        }
 
-		padding-top: 50px
-	}
-	.dimms{
-		padding-top: 20px;
-		width: 250px;
-			}
-	input{
-		float:right;
+        .dimms {
+            padding-top: 20px;
+            width: 250px;
+        }
 
-	}
+        input {
+            float: right;
+        }
 
-	#imgs {
-			float: right; 
+        #imgs {
+            float: right;
+        }
 
-	}
-	#imgs img{
-		display: block;
-	}
+        #imgs img {
+            display: block;
+        }
+    </style>
+    <form method="post" action="posted.php">
+        <h1></h1>
+        <div class="container">
+            <div id="imgs">
+                <img ng-src="{{selectedStyle.image}}" />
+                <img ng-src="{{selectedFlute.image}}" />
+            </div>
+            <h3>Select Style</h3>
+            <select ng-model="selectedStyle" ng-options="x.type for x in styles"></select>
+            <h3>Select Flute</h3>
+            <select ng-model="selectedFlute" ng-options="x.flute for x in flutes"></select>
+            <h3>Select Grade</h3>
+            <select ng-model="selectedGrade" ng-options="x.type for x in grades"></select>
+            <h3>Select Liner</h3>
+            <select ng-model="selectedLiner" ng-options="x.grade for x in liners"></select>
 
-</style>
-<form method="post" action="posted.php">
-<h1></h1>
-<div class="container">
-<div id="imgs">
-<img ng-src="{{selectedStyle.image}}"/>
-<img ng-src="{{selectedFlute.image}}"/>
-</div>
-<h3>Select Style</h3>
-<select ng-model="selectedStyle" ng-options="x.type for x in styles"></select>
-<h3>Select Flute</h3>
-<select ng-model="selectedFlute" ng-options="x.flute for x in flutes"></select>
-<h3>Select Grade</h3>	
-<select ng-model="selectedGrade" ng-options="x.type for x in grades"></select>
-<h3>Select Liner</h3>
-<select ng-model="selectedLiner" ng-options="x.grade for x in liners"></select>	
+            <div class="dimms">
+                <h2>Carton Dimms</h2>
+                <p>Length: <input type="text" ng-model="length"></p>
 
-<div class="dimms">
-<h2>Carton Dimms</h2>
-<p>Length: <input type="text" ng-model="length" ></p>
+                <p>Breadth: <input type="text" ng-model="breadth"></p>
 
-<p>Breadth: <input type="text" ng-model="breadth" ></p>
+                <p>Height: <input type="text" ng-model="height"></p>
 
-<p>Height: <input type="text" ng-model="height" ></p>
+                <p>Qty: <input type="text" ng-model="qty"></p>
 
-<p>Qty: <input type="text" ng-model="qty"></p>
+                <h2>Costing</h2>
+                <p>£ per SqM: <input type="text" ng-model="cost"></p>
+                <p>Labour: <input type="text" ng-model="labour"></p>
+            </div>
 
-<h2>Costing</h2>
-<p>£ per SqM: <input type="text" ng-model="cost"></p>
-<p>Labour: <input type="text" ng-model="labour"></p>
-</div>
+            <br/>
+            <h3>---------------------------Summary----------------------------------------</h3>
+            <br />
+            <p>
+                <h3>Style: {{selectedStyle.type}} </h3>
+                <input type="hidden" name="style" value="{{selectedStyle.type}}">
+                <!--SUMMARY OF CARTON SPEC-->
+                <h3>Grade: {{selectedFlute.flute}}{{selectedGrade.type}}{{selectedLiner.grade}} </h3>
+                <input type="hidden" name="details" value="{{selectedGrade.type}}/{{selectedFlute.flute}}/{{selectedLiner.grade}}">
 
-<br/>
-<h3>---------------------------Summary----------------------------------------</h3>
-<br />
-<p><h3>Style: {{selectedStyle.type}} </h3>
-<input type="hidden" name="style" value="{{selectedStyle.type}}">
-<!--SUMMARY OF CARTON SPEC-->
-<h3>Grade: {{selectedFlute.flute}}{{selectedGrade.type}}{{selectedLiner.grade}} </h3>
-<input type="hidden" name="details" value="{{selectedGrade.type}}/{{selectedFlute.flute}}/{{selectedLiner.grade}}">
-
-<!--calculation for the sheet board size Height + Base X l * 2 + b * 2 + 25 -->
-<h3>Blank Size: <span>{{(+height) * (+selectedStyle.height) ++ (+breadth) * 
-(+selectedStyle.width) ++ (+selectedFlute.thickness) * 
-(+selectedStyle.trimWidth)}} X 
-{{(+length) * (+selectedStyle.length) 
-++ (+breadth) * (+selectedStyle.breadth) 
+                <!--calculation for the sheet board size Height + Base X l * 2 + b * 2 + 25 -->
+                <h3>Blank Size: <span>{{(+height) * (+selectedStyle.height) ++ (+breadth) *
+(+selectedStyle.width) ++ (+selectedFlute.thickness) *
+(+selectedStyle.trimWidth)}} X
+{{(+length) * (+selectedStyle.length)
+++ (+breadth) * (+selectedStyle.breadth)
 ++ (+selectedFlute.thickness) * (+selectedStyle.trimLength) ++ (+selectedStyle.glueFlap)}}</span></h3>
 
-<!--SQUARE M PER CARTON-->
-<h3>Square M per box: {{(height * selectedStyle.height ++ breadth * selectedStyle.width ++ selectedFlute.thickness*3)
- * (length * selectedStyle.length 
-++ breadth * selectedStyle.breadth
-++ selectedFlute.thickness * 8 ++ selectedStyle.glueFlap) / 1000000}}</h3>
+                <!--SQUARE M PER CARTON-->
+                <h3>Square M per box: {{calcSqMperBox()}}</h3>
 
-<!--CALCULATE THE TOTAL SQUARE M OF BOARD REQUIRED-->
-<h3>Total Square M: {{(height * selectedStyle.height ++ breadth * selectedStyle.width ++ selectedFlute.thickness*3)
- * (length * selectedStyle.length 
-++ breadth * selectedStyle.breadth
-++ selectedFlute.thickness * 8 ++ selectedStyle.glueFlap) / 1000000 * qty}}</h3>
+                <!--CALCULATE THE TOTAL SQUARE M OF BOARD REQUIRED-->
+                <h3>Total Square M:
+                    <span ng-if="calcSqMperBoxQty() !== null">{{calcSqMperBoxQty()}}</span>
+                    <span ng-if="calcSqMperBoxQty() === null">Can't do that yet</span>
+                </h3>
 
 
-<label>L: {{length}}</label><br/>
-<label>B: {{breadth}}</label><br/>
-<label>H: {{height}}</label>
-<br/>
+                <label>L: {{length}}</label><br/>
+                <label>B: {{breadth}}</label><br/>
+                <label>H: {{height}}</label>
+                <br/>
 
-<img ng-src="{{selectedStyle.image}}"/>
+                <img ng-src="{{selectedStyle.image}}" />
 
 
-<script src ="scripts\myApp.js"></script>
+                <script src="scripts\myApp.js"></script>
 
 </body>
+
 </html>
--- scripts/myApp.js
+++ scripts/myApp.js
@@ -1,68 +1,65 @@
-var app = angular.module('quoteApp',[]);
-app.controller('styleController', ['$scope', function($scope){
-	$scope.styles = [{
-	type: "0201",
-	height: 1,
-	width: 1,
-	length: 2,
-	breadth: 2,
-	trimWidth: 3,
-	trimLength: 8,
-	glueFlap: 25,
-	image: "/images/0201.png",
-},
-{
-	type: '0203',
-	height: 1,
-	width: 2,
-	length: 2,
-	breadth: 2,
-	trimWidth: 9,
-	trimLength: 40,
-	image: "/images/0203.png",
-},
-{
-	type: '0401',
-	height: 1,
-	depth: 2,
-	length: 2,
-	breadth: 2,
-	trimWidth: 9,
-	trimLength: 40,
-	image: "/images/0401.png",
-}
-];
-$scope.grades=[{
+var app = angular.module('quoteApp', []);
+app.controller('styleController', function($scope, $http) {
+    $scope.styles = null;
+    $scope.grades = [{
 
-	type: "125K",
+        type: "125K",
 
-}];
-$scope.flutes=[{
-	flute: "B",
-	thickness: 3,
-	image: "/images/bFlute.png",
-},
-{
-	flute: "BC",
-	thickness: 7,
-	image: "/images/bcFlute.png",
-},
-{
-	flute: "E",
-	thickness: 2,
-	image: "/images/eFlute.png",
-},
-{
-	flute: "C",
-	thickness: 4,
-	image: "/images/cFlute.png",
-}];
+    }];
+    $scope.flutes = [{
+            flute: "B",
+            thickness: 3,
+            image: "/images/bFlute.png",
+        },
+        {
+            flute: "BC",
+            thickness: 7,
+            image: "/images/bcFlute.png",
+        },
+        {
+            flute: "E",
+            thickness: 2,
+            image: "/images/eFlute.png",
+        },
+        {
+            flute: "C",
+            thickness: 4,
+            image: "/images/cFlute.png",
+        }
+    ];
 
-$scope.liners=[{
-	grade: "125T",
-},
-{
-	grade: "125K",
-}];
+    $scope.liners = [{
+            grade: "125T",
+        },
+        {
+            grade: "125K",
+        }
+    ];
 
-}]);
+    $scope.height = null;
+
+    $scope.calcSqMperBox = function(){
+        try {
+        return ($scope.height * $scope.selectedStyle.height + $scope.breadth * $scope.selectedStyle.width + $scope.selectedFlute.thickness*3)
+            * ($scope.length * $scope.selectedStyle.length
+            + $scope.breadth * $scope.selectedStyle.breadth
+            + $scope.selectedFlute.thickness * 8 + $scope.selectedStyle.glueFlap) / 1000000;
+        } catch(x) {}
+    };
+
+    $scope.calcSqMperBoxQty = function(){
+        var res = $scope.calcSqMperBox() * $scope.qty;
+        if (isNaN(res)) {
+            return null;
+        }
+        return res;
+    };
+
+    $http({
+        method: 'GET',
+        url: '/styles.json.php'
+    }).then(function(response){
+        $scope.styles = response.data;
+    });
+
+});
